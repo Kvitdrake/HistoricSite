223670ab4fcd66a3b261efc11ae8c4c35f05457a
Kvitdrake <142722843+Kvitdrake@users.noreply.github.com>
2023-11-25 20:13:56 +0300

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -2,16 +2,16 @@
 {
     public class User
     {
+        [Key]
         private protected string Id { get; set; }
         private protected string Name { get; set; }
         private protected int Age { get; set; }
         private protected string Email { get; set; }
         private protected string Password { get; set; }
         DateTime RegisterDate { get; set; }
-        public User(string id, string name, int age, string email, string passvord, DateTime registerDate)
+        public User(string name, int age, string email, string passvord, DateTime registerDate)
         {
             Name = name;
-            Id = id;
             Age = age;
             Email = email;
             Password = passvord;
