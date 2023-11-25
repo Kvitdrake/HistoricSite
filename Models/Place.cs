602004aa4b7835dbc66d7c98ad355bce756b70e9
Kvitdrake <142722843+Kvitdrake@users.noreply.github.com>
2023-11-25 19:49:03 +0300

    Update Place.cs

--- Models/Place.cs
+++ Models/Place.cs
@@ -4,6 +4,7 @@ namespace HistoricSite.Models
 {
     public class Place
     {
+        [Key] 
         public int Id { get; set; }
         public string? Name { get; set; }
         [DataType(DataType.Date)]
